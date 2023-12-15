aba67c264aebb74ebd000a4134106c7fcecef2bc
Mahaarta <minataputra@gmail.com>
2023-12-15 10:42:45 +0700

    Initial Commit

---
+++ Viper Architecture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Viper Architecture
+//
+//  Created by Minata on 15/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Viper Architecture/Viper_ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Viper_ArchitectureApp.swift
+//  Viper Architecture
+//
+//  Created by Minata on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Viper_ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
