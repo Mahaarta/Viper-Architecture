cf8d27c2c3819fd2e7184b6a8bd3d018fa2d2dc7
Mahaarta <minataputra@gmail.com>
2023-12-22 19:28:31 +0700

    feat: Network Unit Test

--- Viper Architecture/Module/Login/Interactor/LoginInteractor.swift
+++ Viper Architecture/Module/Login/Interactor/LoginInteractor.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Alamofire
+import RxSwift
 
 class LoginInteractor: LoginPresenterToInteractorProtocol {
     
@@ -17,31 +18,11 @@ class LoginInteractor: LoginPresenterToInteractorProtocol {
         return LoginBaseEndpoint.getBasePath() + "auth/"
     }
     
-    func loginProcess(username: String, password: String) {
-        let endpoint = getPath() + "login"
+    func loginProcess(username: String, password: String) -> Observable<LoginEntity?> {
+        let endpoint = URL(string: getPath() + "login")!
         let parameters: [String: Any] = ["username": username, "password": password]
         
-        AF.request(
-            endpoint,
-            method: .post,
-            parameters: parameters,
-            encoding: JSONEncoding.default
-        ).response { response in
-            
-            if response.response?.statusCode == 200 {
-                guard let data = response.data else { return }
-                do {
-                    let decoder = JSONDecoder()
-                    let loginResponse = try decoder.decode(LoginEntity.self, from: data)
-                    guard let _ = loginResponse.token else { return }
-                    self.presenter?.loginSuccess()
-                } catch let error {
-                    print(error)
-                }
-                
-            } else {
-                self.presenter?.loginFailed()
-            }
-        }
+        let data: Observable<LoginEntity?> = NetworkManager.shared.excuteQueryWithEncoding(url: endpoint, method: .post, parameters: parameters)
+        return data
     }
 }
--- Viper Architecture/Module/Login/Protocols/Presenter to Interactor/LoginPresenterToInteractorProtocol.swift
+++ Viper Architecture/Module/Login/Protocols/Presenter to Interactor/LoginPresenterToInteractorProtocol.swift
@@ -5,10 +5,12 @@
 //  Created by Minata on 19/12/2023.
 //
 
+import RxSwift
+
 protocol LoginPresenterToInteractorProtocol: Any {
     
     var presenter: LoginInteractorToPresenterProtocol? { get set }
     var loginSuccess: Bool? { get }
     
-    func loginProcess(username: String, password: String)
+    func loginProcess(username: String, password: String) -> Observable<LoginEntity?>
 }
